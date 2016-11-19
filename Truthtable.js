3e5a4524f01a96fa26582a4372f956016c97dc11
Jason Liu <j-lhy@hotmail.com>
2016-11-20 03:02:19 +0800

    Add some js comment

--- Truthtable.js
+++ Truthtable.js
@@ -1,24 +1,37 @@
 /**
  * 
  */
-function NotationConverter() {}
+function NotationConverter() { }
 NotationConverter.prototype = new Object();
-NotationConverter.prototype.keywords = [ "and", "or", "not", "=>", "<=>" ];
+NotationConverter.prototype.keywords = ["and", "or", "not", "=>", "<=>"];
 
-NotationConverter.prototype.isKeywords = function(word) {
+/**
+ * 
+ * 
+ * @param {any} word
+ * @returns
+ */
+NotationConverter.prototype.isKeywords = function (word) {
 	var result = this.keywords.indexOf(word) !== -1;
 	if (result)
 		console.log("found keyword : " + word);
-		
+
 	return result;
 };
 
-NotationConverter.prototype.getVariableList = function(postfixExpression) {
+
+/**
+ * Get variable list inside a postfix expression
+ * 
+ * @param {String} postfixExpression - post fix expression string, e.g. "a b and"
+ * @returns an array of variable name, e.g. ["a", "b"]
+ */
+NotationConverter.prototype.getVariableList = function (postfixExpression) {
 	var tokens = postfixExpression.split(" ");
 	var theConverter = this;
 
 	var set = [];
-	tokens.forEach(function(element) {
+	tokens.forEach(function (element) {
 		if (!theConverter.isKeywords(element)) {
 			set[element] = true;
 		}
@@ -32,48 +45,73 @@ NotationConverter.prototype.getVariableList = function(postfixExpression) {
 	return list;
 }
 
-// lowest number = higher precedence...
-NotationConverter.prototype.getPrecedence = function(operator) {
+
+/**
+ * Get the precedence of operator, lowest number = higher precedence
+ * 
+ * @param {String} operator
+ * @returns
+ */
+NotationConverter.prototype.getPrecedence = function (operator) {
 	var precedence;
 	switch (operator) {
-	case "not":
-		precedence = 1;
-		break;
-	case "and":
-		precedence = 2;
-		break;
-	case "or":
-		precedence = 3;
-		break;
-	case "=>":
-		precedence = 4;
-		break;
-	case "<=>":
-		precedence = 5;
-		break;
-	default:
-		precedence = 99; // this is for (....
-		break;
+		case "not":
+			precedence = 1;
+			break;
+		case "and":
+			precedence = 2;
+			break;
+		case "or":
+			precedence = 3;
+			break;
+		case "=>":
+			precedence = 4;
+			break;
+		case "<=>":
+			precedence = 5;
+			break;
+		default:
+			precedence = 99; // this is for (....
+			break;
 	}
 
 	return precedence;
 };
 
-NotationConverter.prototype.isHigherPrecedence = function(operatorA, operatorB) {
+/**
+ * 
+ * 
+ * @param {any} operatorA
+ * @param {any} operatorB
+ * @returns
+ */
+NotationConverter.prototype.isHigherPrecedence = function (operatorA, operatorB) {
 	var higherPrecedenceResult;
 
 	higherPrecedenceResult = this.getPrecedence(operatorA) < this
-			.getPrecedence(operatorB);
+		.getPrecedence(operatorB);
 
 	// return (higherPrecedenceResult > 0)
 	return higherPrecedenceResult;
 };
 
-NotationConverter.prototype.convert = function(infixExpression) {
+/**
+ * Convert a infix expression into a postfix expression
+ * 
+ * @param {String} infixExpression - "a and b"
+ * @returns a postfix expression - "a b and"
+ */
+NotationConverter.prototype.convert = function (infixExpression) {
 	var postfix = "";
 	var pattern = /(\(|\)| )/g;
 	var arrayStrings = infixExpression.split(pattern);
-	var arrayWithoutSpace = arrayStrings.filter(function(element){
+	/**
+	 * 
+	 * 
+	 * @param {any} element
+	 * @returns
+	 */
+	var arrayWithoutSpace = arrayStrings.filter(function (element) {
 		return element != ' ' && element != "";
 	});
 	var stack = [];
@@ -96,7 +134,7 @@ NotationConverter.prototype.convert = function(infixExpression) {
 					if (this.isHigherPrecedence(top, token)) {
 						postfix += stack.pop() + " ";
 						console.log(top + " has higher precedence than "
-								+ token + " pop & added to string");
+							+ token + " pop & added to string");
 					} else {
 						break;
 					}
@@ -105,12 +143,12 @@ NotationConverter.prototype.convert = function(infixExpression) {
 				stack.push(token);
 			}
 
-		} else if (token === '(') { 
+		} else if (token === '(') {
 			stack.push(token);
-		} else if (token === ')'){
-			while (stack.length > 0){
-				var top = stack[stack.length -1];
-				if (top === '('){
+		} else if (token === ')') {
+			while (stack.length > 0) {
+				var top = stack[stack.length - 1];
+				if (top === '(') {
 					stack.pop();
 					break;
 				} else {
@@ -138,70 +176,82 @@ NotationConverter.prototype.convert = function(infixExpression) {
 /**
  * Generate template of truth table without result
  */
-function TruthtableGenerator(){
-	
-} 
+function TruthtableGenerator() {}
 /**
- *  
- * @param variableList variable list for the variable in the truth table, e.g. ["a","b"]
- * @return generate a truth table. [{"a":true, "b":true} , {"a":false, "b":false}];
+ * Generate a truth table with every condition
+ * 
+ * @param {Array} variableList - variableList variable list for the variable in the truth table, e.g. ["a","b"]
+ * @returns a truth table. [{"a":true, "b":true} , {"a":false, "b":false}]
  */
-TruthtableGenerator.prototype.generate = function(variableList){
+TruthtableGenerator.prototype.generate = function (variableList) {
 	var count = variableList.length;
 	var lastIndex = count - 1;
 	var totalRow = Math.pow(2, count);
-	
+
 	var numberOfPair; // the longest true-true or true-true-true-true, false-false, false-false-false-false pair along the y axis in a column
 	var cellValue;
-	
+
 	var truthtable = [];
-	for (var rowIndex = 0; rowIndex < totalRow ; rowIndex++){
+	for (var rowIndex = 0; rowIndex < totalRow; rowIndex++) {
 		var row = [];
-		for (var columnIndex = 0; columnIndex < variableList.length; columnIndex++){
+		for (var columnIndex = 0; columnIndex < variableList.length; columnIndex++) {
 			numberOfPair = Math.pow(2, lastIndex - columnIndex);
 			var trueFalseSectionIndex = parseInt(rowIndex / numberOfPair);
 			cellValue = trueFalseSectionIndex % 2 === 0;
-			
-			row[ variableList[columnIndex] ] = cellValue; 
+
+			row[variableList[columnIndex]] = cellValue;
 		}
 		truthtable.push(row);
 	}
-	
+
 	return truthtable;
 };
 
 
 /**
- * 
+ * Evaluator that evaluate a truth table with result
  */
-// Evaluator
-function PostfixEvalutor() {
-}
+function PostfixEvalutor() {}
+
 PostfixEvalutor.prototype = new Object();
-PostfixEvalutor.prototype.setTruthtable = function(truthtable) {
+
+/**
+ * 
+ *  
+ * @param {any} truthtable
+ */
+PostfixEvalutor.prototype.setTruthtable = function (truthtable) {
 	this.truthtable = truthtable;
 };
-PostfixEvalutor.prototype.getTruthtable = function() {
+
+/**
+ * 
+ * 
+ * @returns
+ */
+PostfixEvalutor.prototype.getTruthtable = function () {
 	return this.truthtable;
 };
 
-// tokenArray = ["a", "not", "b", "and"]
-// row = ["a":true, "b":false]
-PostfixEvalutor.prototype.evaluteRow = function(tokenArray, row) {
+/**
+ * Evaluate result for a row on the truth table
+ * 
+ * @param {any} tokenArray - The post fix evaluation token to be evaluted, e.g. ["a", "not", "b", "and"]
+ * @param {any} row - The value of a and b on a row of truth table, e.g. ["a":true, "b":false]
+ * @returns The result of the post fix evaluation on that row
+ */
+PostfixEvalutor.prototype.evaluteRow = function (tokenArray, row) {
 	var stack = [];
 	var result;
 	var valA;
 	var valB;
 
-	tokenArray.forEach(function(element) {
-		console.log("pushing postfix token: " + element);
+	tokenArray.forEach(function (element) {
 		stack.push(element);
 
 		var token = stack.pop();
-		console.log("poping token: " + token);
 		if (token === "not") {
 			valB = stack.pop();
-			console.log("valB in not " + valB);
 			result = !valB
 		} else if (token === "or") {
 			valB = stack.pop();
@@ -211,12 +261,12 @@ PostfixEvalutor.prototype.evaluteRow = function(tokenArray, row) {
 			valB = stack.pop();
 			valA = stack.pop();
 			result = valA && valB;
-		} else if (token === "=>"){
+		} else if (token === "=>") {
 			valB = stack.pop();
 			valA = stack.pop();
 			result = !(valA && !valB);  // only (A = T, B = F) will be false, so not them => true
 		}
-		else if (token === "<=>" ) {
+		else if (token === "<=>") {
 			valB = stack.pop();
 			valA = stack.pop();
 			result = valA === valB;
@@ -229,7 +279,12 @@ PostfixEvalutor.prototype.evaluteRow = function(tokenArray, row) {
 
 	return stack.pop();
 };
-PostfixEvalutor.prototype.evalute = function(postfixExpression) {
+
+/**
+ * Evaludate the postfix expression. The evaluation result will be stored at "result" of each row object
+ * @param {String} postfix Expression - Post fix expression string, e.g. "a b and"
+ */
+PostfixEvalutor.prototype.evalute = function (postfixExpression) {
 	var tokenArray = postfixExpression.split(" ");
 	var row;
 	var truthtable = this.getTruthtable();
@@ -241,66 +296,71 @@ PostfixEvalutor.prototype.evalute = function(postfixExpression) {
 	}
 }
 
+
+
+function TruthtableUI() { }
 /**
+ * Generate a truth table inside a div
+ *
  * 
- * infixInput : infix formula string
- * outputDiv : the id of the div
- * generatedTableId : the table id to be generated
- * format : the representation format of the cell value
- *      undefined : true, false
- *      'UPPER_LETTER' : T, F
- *      'DIGIT' : 1, 0
+ * 
+ * @param {String} infixInput - infix formula string
+ * @param {String} outputDiv - the id of the div
+ * @param {String} generatedTableId - the table id to be generated
+ * @param {any} format - the representation format of the cell value
+ * 				format parameter - representation format:
+ * 				undefined - true, false
+ * 				'UPPER_LETTER' - T, F
+ * 				'DIGIT' - 1, 0
  */
-function TruthtableUI(){}
-TruthtableUI.prototype.generate = function(infixInput, outputDiv, generatedTableId, format){		
-	
+TruthtableUI.prototype.generate = function (infixInput, outputDiv, generatedTableId, format) {
+
 	// convert infix from postfix to infix
 	// and get the variablelist
 	var converter = new NotationConverter();
 	var postfix = converter.convert(infixInput);
 	var variableList = converter.getVariableList(postfix);
-	
+
 	// generate true and false table
 	var truthtableGenerator = new TruthtableGenerator();
 	var truthtable = truthtableGenerator.generate(variableList);
-	
+
 	// evalute the result
 	var evalutor = new PostfixEvalutor();
 	evalutor.setTruthtable(truthtable);
 	evalutor.evalute(postfix);
-	
+
+	// generate the UI
 	var resultTruthtable = evalutor.getTruthtable();
-	
-	var tableContent = '<table class="table table-bordered table-hover text-center" id="' +generatedTableId + '" >';
+	var tableContent = '<table class="table table-bordered table-hover text-center" id="' + generatedTableId + '" >';
 	tableContent += '<thead><tr>'
-	for (var i = 0; i < variableList.length; i++){
-		tableContent += '<th class="text-center">' + variableList[i] + '</th>'; 
+	for (var i = 0; i < variableList.length; i++) {
+		tableContent += '<th class="text-center">' + variableList[i] + '</th>';
 	}
 	tableContent += '<th class="text-center">' + infixInput + '</th>';
 	tableContent += '</tr></thead>';
-	
+
 	tableContent += '<tbody>';
-	for (var i = 0; i < resultTruthtable.length; i++){
+	for (var i = 0; i < resultTruthtable.length; i++) {
 		var row = resultTruthtable[i];
 		tableContent += '<tr>'
-		for (var key in row){
-            // value in table cell
-            var value;
-            if (format == 'undefined' || format === undefined){
-                value = row[key];
-            } else if (format === 'UPPER_LETTER'){
-                value = (row[key] === true) ? 'T' : 'F';
-            } else if (format === 'DIGIT'){
-                value = (row[key] === true) ? '1' : '0';
-            }
-            
-			tableContent += '<td>' + value + '</td>';	
+		for (var key in row) {
+			// value in table cell
+			var value;
+			if (format == 'undefined' || format === undefined) {
+				value = row[key];
+			} else if (format === 'UPPER_LETTER') {
+				value = (row[key] === true) ? 'T' : 'F';
+			} else if (format === 'DIGIT') {
+				value = (row[key] === true) ? '1' : '0';
+			}
+
+			tableContent += '<td>' + value + '</td>';
 		}
 		tableContent += '</tr>';
 	}
 	tableContent += '</tbody>';
 	tableContent += '</table>';
-	//alert(tableContent);
-	
+
 	document.getElementById(outputDiv).innerHTML = tableContent;
 }
